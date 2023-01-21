80263eddef841125ec250becd993a78eafba6568
zhanglun <zhanglun1410@gmail.com>
2023-01-21 15:52:47 +0800

    improvement: add Toolbar

--- src/pages/layout.rs
+++ src/pages/layout.rs
@@ -35,9 +35,9 @@ pub struct Props {
 #[function_component(Layout)]
 pub fn layout (props: &Props) -> Html {
   html! {
-    <div class="w-full h-screen grid gap-2 grid-rows-[58px_1fr]">
-      <div class="flex justify-center pt-2 sticky top-0 z-10">
-        <nav class="p-1 flex items-center justify-start rounded-full bg-white shadow-md">
+    <div class="w-full grid gap-2 grid-rows-[58_1fr]">
+      <div class="flex justify-center p-2 mt-4 sticky top-0 z-10">
+        <nav class="py-2 px-1 flex items-center justify-start rounded-full bg-white shadow-md">
           <a class="inline-block text-center text-base text-black hover:text-black hover:bg-slate-100 px-3 py-2 mx-1 rounded-full" href={"/"}>{"Home"}</a>
           <a class="inline-block text-center text-base text-black hover:text-black hover:bg-slate-100 px-3 py-2 mx-1 rounded-full" href={"/pexels"}>{"Pexels"}</a>
           <a class="inline-block text-center text-base text-black hover:text-black hover:bg-slate-100 px-3 py-2 mx-1 rounded-full" href={"/unsplash"}>{"Unsplash"}</a>
