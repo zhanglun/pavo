d90d02a5579f77c930d95197d4351cd38fbb5b10
zhanglun <zhanglun1410@gmail.com>
2023-01-27 15:37:05 +0800

    improvement: add tray

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -7,10 +7,26 @@ mod cmd;
 mod config;
 mod services;
 
+use tauri::{CustomMenuItem, SystemTray, SystemTrayMenu, SystemTrayMenuItem};
+
+fn create_tray () -> SystemTray {
+  let quit = CustomMenuItem::new("quit".to_string(), "Quit");
+  let hide = CustomMenuItem::new("hide".to_string(), "Hide");
+  let tray_menu = SystemTrayMenu::new()
+    .add_item(quit)
+    .add_native_item(SystemTrayMenuItem::Separator)
+    .add_item(hide);
+
+  let tray = SystemTray::new().with_menu(tray_menu);
+
+  tray
+}
+
 fn main() {
   config::PavoConfig::create_app_folder().expect("create app folder failed!");
 
   tauri::Builder::default()
+    .system_tray(create_tray())
     .invoke_handler(tauri::generate_handler![
       cmd::set_as_desktop,
       cmd::download,
