5d2af7fae7f9994bb22085138ed753994e3df61f
zhanglun <zhanglun1410@gmail.com>
2023-04-15 08:23:25 +0800

    fix: fix dark mode

--- src/app.rs
+++ src/app.rs
@@ -1,4 +1,5 @@
 use wasm_bindgen::prelude::*;
+use weblog::console_log;
 use yew::prelude::*;
 
 use crate::pages::layout::Layout;
@@ -9,8 +10,33 @@ extern "C" {
   async fn invoke(cmd: &str, args: JsValue) -> JsValue;
 }
 
+
+
 #[function_component(App)]
 pub fn app() -> Html {
+  let window = web_sys::window().unwrap();
+  let local_storage = web_sys::window().unwrap().local_storage().unwrap().unwrap();
+  let theme = local_storage.get_item("theme").unwrap();
+  let document = web_sys::window().unwrap().document().unwrap();
+
+  console_log!(document);
+  console_log!(local_storage);
+
+  // if theme == "dark" || (!("theme" in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+  //   document.documentElement.classList.add('dark')
+  // } else {
+  //   document.documentElement.classList.remove('dark')
+  // }
+
+  // Whenever the user explicitly chooses light mode
+  // local_storage.theme = "light";
+
+  // // Whenever the user explicitly chooses dark mode
+  // local_storage.theme = "dark";
+
+  // // Whenever the user explicitly chooses to respect the OS preference
+  // local_storage.removeItem("theme");
+
   html! {
     <Layout>
     </Layout>
--- src/components/toolbar.rs
+++ src/components/toolbar.rs
@@ -96,14 +96,36 @@ pub fn toolbar(props: &Props) -> Html {
         grid gap-2 grid-flow-col
       ">
         <span
-          class="w-4 h-4 py-1 text-center text-neutral-800 cursor-pointer transition-all origin-center hover:text-black hover:scale-110"
+          class="
+            py-1
+            text-center
+            cursor-pointer
+            transition-[scale]
+            origin-center
+            text-neutral-800
+            dark:text-white
+            hover:text-black
+            dark:hover:text-white
+            hover:scale-110
+          "
           onclick={set_as_desktop}>
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
             <path stroke-linecap="round" stroke-linejoin="round" d="M9 17.25v1.007a3 3 0 01-.879 2.122L7.5 21h9l-.621-.621A3 3 0 0115 18.257V17.25m6-12V15a2.25 2.25 0 01-2.25 2.25H5.25A2.25 2.25 0 013 15V5.25m18 0A2.25 2.25 0 0018.75 3H5.25A2.25 2.25 0 003 5.25m18 0V12a2.25 2.25 0 01-2.25 2.25H5.25A2.25 2.25 0 013 12V5.25" />
           </svg>
         </span>
         <span
-          class="w-4 h-4 py-1 text-center text-neutral-800 cursor-pointer transition-all origin-center hover:text-black hover:scale-110"
+          class="
+            py-1
+            text-center
+            cursor-pointer
+            transition-[scale]
+            origin-center
+            text-neutral-800
+            dark:text-white
+            hover:text-black
+            dark:hover:text-white
+            hover:scale-110
+          "
           onclick={download}>
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -116,7 +138,18 @@ pub fn toolbar(props: &Props) -> Html {
             </svg>
         </span>
         <a
-          class="w-4 h-4 py-1 text-center text-neutral-800 cursor-pointer transition-all origin-center hover:text-black hover:scale-110"
+          class="
+            py-1
+            text-center
+            cursor-pointer
+            transition-[scale]
+            origin-center
+            text-neutral-800
+            dark:text-white
+            hover:text-black
+            dark:hover:text-white
+            hover:scale-110
+          "
           target="_blank"
           href={props.href.clone()}
         >
