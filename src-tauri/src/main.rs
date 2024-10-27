da02b6fe5c347cf20f503e897fcda9df8d787115
zhanglun <zhanglun1410@gmail.com>
2024-10-27 20:08:38 +0800

    "migrate in window":

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -11,115 +11,112 @@ mod services;
 
 use cmd::AsyncProcInputTx;
 use services::AsyncProcessMessage;
-use tauri::{
-  AppHandle, CustomMenuItem, GlobalWindowEvent, Manager, SystemTray, SystemTrayEvent,
-  SystemTrayMenu, SystemTrayMenuItem, WindowEvent, Wry,
-};
+use tauri::{AppHandle, Manager, tray::TrayIconBuilder, WindowEvent};
 use tokio::sync::{mpsc, Mutex};
 
-fn create_tray() -> SystemTray {
-  let show = CustomMenuItem::new("show".to_string(), "Show");
-  let hide = CustomMenuItem::new("hide".to_string(), "Hide");
-  let quit = CustomMenuItem::new("quit".to_string(), "Quit");
-
-  // let auto_rotate = CustomMenuItem::new("auto_rotate".to_string(), "Auto Rotate");
-  let previous_photo = CustomMenuItem::new("previous_photo".to_string(), "Previous Photo");
-  let next_photo = CustomMenuItem::new("next_photo".to_string(), "Next Photo");
-
-  let tray_menu = SystemTrayMenu::new()
-    // .add_item(auto_rotate)
-    .add_item(previous_photo)
-    .add_item(next_photo)
-    .add_native_item(SystemTrayMenuItem::Separator)
-    .add_item(show)
-    .add_item(hide)
-    .add_native_item(SystemTrayMenuItem::Separator)
-    .add_item(quit);
-
-  SystemTray::new().with_menu(tray_menu)
-}
-
-fn handle_tray_event(
-  app: &AppHandle,
-  event: SystemTrayEvent,
-  sender: mpsc::Sender<AsyncProcessMessage>,
-) {
-  match event {
-    SystemTrayEvent::DoubleClick {
-      tray_id,
-      position,
-      size,
-      ..
-    } => {
-      let window = app.get_webview_window("main").unwrap();
-      window.show().unwrap();
-    }
-    SystemTrayEvent::MenuItemClick { id, .. } => match id.as_str() {
-      "previous_photo" => {
-        let tx = sender.clone();
-        tokio::spawn(async move {
-          tx.send(AsyncProcessMessage::PreviousPhoto).await.unwrap();
-        });
-      }
-      "next_photo" => {
-        let tx = sender.clone();
-        tokio::spawn(async move {
-          tx.send(AsyncProcessMessage::NextPhoto).await.unwrap();
-          println!("send");
-        });
-      }
-      "show" => {
-        let window = app.get_webview_window("main").unwrap();
-        window.show().unwrap();
-      }
-      "hide" => {
-        let window = app.get_webview_window("main").unwrap();
-        window.hide().unwrap();
-      }
-      "quit" => {
-        std::process::exit(0);
-      }
-      _ => {}
-    },
-    _ => {}
-  };
-}
-
-fn handle_window_event(event: GlobalWindowEvent<Wry>) {
-  let window = event.window();
-  let app = window.app_handle();
-
-  match event.event() {
-    WindowEvent::CloseRequested { api, .. } => {
-      let window = window.clone();
-      api.prevent_close();
-      window.hide().unwrap();
-      // ask the user if he wants to quit
-      // ask(
-      //   Some(&window),
-      //   "Tauri API",
-      //   "Are you sure that you want to close this window?",
-      //   |answer| {
-      //     if answer {
-      //       // .close() cannot be called on the main thread
-      //       std::thread::spawn(move || {
-      //         window.close().unwrap();
-      //       });
-      //     }
-      //   },
-      // );
-    }
-    WindowEvent::Focused(_) => {
-      println!("window focused!");
-
-      // tauri::async_runtime::spawn(async move {
-      //   let mut g_cache = cache::CACHE.lock().await;
-      //   g_cache.update_timestamp();
-      // });
-    }
-    _ => {}
-  }
-}
+//fn create_tray() -> SystemTray {
+//  let show = CustomMenuItem::new("show".to_string(), "Show");
+//  let hide = CustomMenuItem::new("hide".to_string(), "Hide");
+//  let quit = CustomMenuItem::new("quit".to_string(), "Quit");
+//
+//  // let auto_rotate = CustomMenuItem::new("auto_rotate".to_string(), "Auto Rotate");
+//  let previous_photo = CustomMenuItem::new("previous_photo".to_string(), "Previous Photo");
+//  let next_photo = CustomMenuItem::new("next_photo".to_string(), "Next Photo");
+//
+//  let tray_menu = SystemTrayMenu::new()
+//    // .add_item(auto_rotate)
+//    .add_item(previous_photo)
+//    .add_item(next_photo)
+//    .add_native_item(SystemTrayMenuItem::Separator)
+//    .add_item(show)
+//    .add_item(hide)
+//    .add_native_item(SystemTrayMenuItem::Separator)
+//    .add_item(quit);
+//
+//  SystemTray::new().with_menu(tray_menu)
+//}
+//
+//fn handle_tray_event(
+//  app: &AppHandle,
+//  event: SystemTrayEvent,
+//  sender: mpsc::Sender<AsyncProcessMessage>,
+//) {
+//  match event {
+//    SystemTrayEvent::DoubleClick {
+//      tray_id,
+//      position,
+//      size,
+//      ..
+//    } => {
+//      let window = app.get_webview_window("main").unwrap();
+//      window.show().unwrap();
+//    }
+//    SystemTrayEvent::MenuItemClick { id, .. } => match id.as_str() {
+//      "previous_photo" => {
+//        let tx = sender.clone();
+//        tokio::spawn(async move {
+//          tx.send(AsyncProcessMessage::PreviousPhoto).await.unwrap();
+//        });
+//      }
+//      "next_photo" => {
+//        let tx = sender.clone();
+//        tokio::spawn(async move {
+//          tx.send(AsyncProcessMessage::NextPhoto).await.unwrap();
+//          println!("send");
+//        });
+//      }
+//      "show" => {
+//        let window = app.get_webview_window("main").unwrap();
+//        window.show().unwrap();
+//      }
+//      "hide" => {
+//        let window = app.get_webview_window("main").unwrap();
+//        window.hide().unwrap();
+//      }
+//      "quit" => {
+//        std::process::exit(0);
+//      }
+//      _ => {}
+//    },
+//    _ => {}
+//  };
+//}
+//
+//fn handle_window_event(event: GlobalWindowEvent<Wry>) {
+//  let window = event.window();
+//  let app = window.app_handle();
+//
+//  match event.event() {
+//    WindowEvent::CloseRequested { api, .. } => {
+//      let window = window.clone();
+//      api.prevent_close();
+//      window.hide().unwrap();
+//      // ask the user if he wants to quit
+//      // ask(
+//      //   Some(&window),
+//      //   "Tauri API",
+//      //   "Are you sure that you want to close this window?",
+//      //   |answer| {
+//      //     if answer {
+//      //       // .close() cannot be called on the main thread
+//      //       std::thread::spawn(move || {
+//      //         window.close().unwrap();
+//      //       });
+//      //     }
+//      //   },
+//      // );
+//    }
+//    WindowEvent::Focused(_) => {
+//      println!("window focused!");
+//
+//      // tauri::async_runtime::spawn(async move {
+//      //   let mut g_cache = cache::CACHE.lock().await;
+//      //   g_cache.update_timestamp();
+//      // });
+//    }
+//    _ => {}
+//  }
+//}
 
 #[tokio::main]
 async fn main() {
@@ -179,9 +176,16 @@ async fn main() {
       // scheduler::Scheduler::init(async_process_input_rx);
       #[cfg(target_os = "windows")]
       {
-        println!("asdfasdfasdf");
-        app.tray_handle().set_icon(tauri::Icon::Raw(include_bytes!("../icons/win-icon.png").to_vec())).unwrap();
+        //app
+        //  .tray_handle()
+        //  .set_icon(tauri::Icon::Raw(
+        //    include_bytes!("../icons/win-icon.png").to_vec(),
+        //  ))
+        //  .unwrap();
       }
+      let tray = TrayIconBuilder::new()
+        .icon(app.default_window_icon().unwrap().clone())
+        .build(app)?;
 
       Ok(())
     })
