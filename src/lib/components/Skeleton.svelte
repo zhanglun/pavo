ec2d7fbaa09eae92430106666808c949b42af0e3
zhanglun <zhanglun1410@gmail.com>
2024-12-23 17:28:16 +0800

    feat: add Skeleton

---
+++ src/lib/components/Skeleton.svelte
@@ -0,0 +1,4 @@
+<script lang="ts">
+  import { CardPlaceholder, ImagePlaceholder, ListPlaceholder, Skeleton, TestimonialPlaceholder, TextPlaceholder, VideoPlaceholder, WidgetPlaceholder } from 'flowbite-svelte';
+</script>
+<ImagePlaceholder />
