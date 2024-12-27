4e784d1800d097acbc379c2693ea5a9dbb9a396e
zhanglun <zhanglun1410@gmail.com>
2024-12-27 14:43:10 +0800

    feat: add About page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,7 @@
   import { Tabs, TabItem } from 'flowbite-svelte';
   import Bing from '../pages/bing.svelte';
   import Settings from '../pages/settings.svelte';
+  import About from '../pages/about.svelte';
 </script>
 <Tabs tabStyle="underline" contentClass="p-4">
   <TabItem open title="Bing" >
@@ -9,5 +10,8 @@
   </TabItem>
   <TabItem title="Settings">
     <Settings />
+  </TabItem>
+  <TabItem title="About">
+    <About />
   </TabItem>
 </Tabs>
