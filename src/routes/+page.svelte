b5ec9034a505ad3a62e8ba67e401b87c7a73be6a
zhanglun <zhanglun1410@gmail.com>
2024-12-30 14:34:04 +0800

    improvement: scrollbar style

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -32,7 +32,7 @@
 
 <div class="w-full h-full flex flex-col">
   <Tabs tabStyle="underline" contentClass="flex-1 h-0">
-    <TabItem open={current === "Bing"} title="Bing" divClass="p-4 pr-0 h-full overflow-y-auto overflow-x-hidden scrollbar-stable">
+    <TabItem open={current === "Bing"} title="Bing" divClass="p-4 pr-2 h-full overflow-y-auto overflow-x-hidden scrollbar-stable">
       <Bing />
     </TabItem>
     <TabItem open={current === "Settings"} title="Settings" divClass="p-4 h-full overflow-y-auto overflow-x-hidden scrollbar-stable">
